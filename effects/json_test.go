cd7474bc3d0522d01c2b0e11972197d3949dc5c4
Brian Ewing <brianewing@gmail.com>
2017-09-25 12:51:04 +0100

    Json: add Unmarshal benchmark

--- effects/json_test.go
+++ effects/json_test.go
@@ -5,20 +5,46 @@ import (
 	"redshift/strip"
 )
 
-func BenchmarkMarshal(b *testing.B) {
-	strip := strip.New(60)
-
-	effects := []Effect{
+func animationSet() []Effect {
+	return []Effect{
 		&Clear{},
-		&Brightness{Brightness: 200},
-		&RainbowEffect{Size: 150, Speed: 0},
+		&RainbowEffect{Size: 150, Speed: 1, Dynamic: true},
+		//&Combine{
+		//	Effects: []Effect{
+		//		&Clear{},
+		//		&RainbowEffect{Size: 100, Speed: 1, Dynamic: false},
+		//		&Brightness{Brightness: 200},
+		//	},
+		//},
+		&BlueEffect{},
+		&LarsonEffect{Color: []uint8{0,0,0}},
 	}
+}
+
+func stripAndEffects() (*strip.LEDStrip, []Effect) {
+	strip := strip.New(60)
+	effects := animationSet()
 
 	for _, effect := range effects {
 		effect.Render(strip)
 	}
 
+	return strip, effects
+}
+
+func BenchmarkMarshalJSON(b *testing.B) {
+	_, effects := stripAndEffects()
+
 	for i := 0; i < b.N; i++ {
 		MarshalJson(effects)
+	}
+}
+
+func BenchmarkUnmarshalJSON(b *testing.B) {
+	_, effects := stripAndEffects()
+	effectsJson, _ := MarshalJson(effects)
+
+	for i := 0; i < b.N; i++ {
+		UnmarshalJson(effectsJson)
 	}
 }
