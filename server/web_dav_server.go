55396a0ab1cc0a78b1dfe491d79879998beecd63
Brian Ewing <brianewing@gmail.com>
2017-10-15 11:57:47 +0100

    Add "Pragma: no-cache" header to WebDAV server

--- server/web_dav_server.go
+++ server/web_dav_server.go
@@ -14,7 +14,7 @@ func RunWebDavServer(addr string, directory string) error {
 			log.Println("WD:", r.Method, r.RequestURI, "[" + r.RemoteAddr + "]", "|", err)
 		},
 	}
-	server := &http.Server{Addr: addr, Handler: corsWrapper(webDavHandler)}
+	server := &http.Server{Addr: addr, Handler: corsWrapper(noCacheWrapper(webDavHandler))}
 	return server.ListenAndServe()
 }
 
@@ -23,6 +23,13 @@ func corsWrapper(h http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "Depth, Content-Type")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, PROPFIND, MKCOL, DELETE, PUT, GET")
+		h.ServeHTTP(w, r)
+	})
+}
+
+func noCacheWrapper(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Pragma", "no-cache")
 		h.ServeHTTP(w, r)
 	})
 }
