c11badae0a7d6de4cb625281a6f03b0484be3d79
Brian Ewing <brianewing@gmail.com>
2017-10-15 09:36:38 +0100

    Add CORS headers to WebDAV server

--- server/web_dav_server.go
+++ server/web_dav_server.go
@@ -7,13 +7,22 @@ import (
 )
 
 func RunWebDavServer(addr string, directory string) error {
-	handler := &webdav.Handler{
+	webDavHandler := &webdav.Handler{
 		FileSystem: webdav.Dir(directory),
 		LockSystem: webdav.NewMemLS(),
 		Logger: func(r *http.Request, err error) {
 			log.Println("WD:", r.Method, r.RequestURI, "[" + r.RemoteAddr + "]", "|", err)
 		},
 	}
-	server := &http.Server{Addr: addr, Handler: handler}
+	server := &http.Server{Addr: addr, Handler: corsWrapper(webDavHandler)}
 	return server.ListenAndServe()
+}
+
+func corsWrapper(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Depth, Content-Type")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, PROPFIND, MKCOL, DELETE, PUT, GET")
+		h.ServeHTTP(w, r)
+	})
 }
