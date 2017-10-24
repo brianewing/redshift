6425cadfba32666d8d3c516ad6a9be68cf82e77d
Brian Ewing <brianewing@gmail.com>
2017-10-24 21:24:20 +0100

    Add todos / helper to web_dav_server.go

--- server/web_dav_server.go
+++ server/web_dav_server.go
@@ -4,18 +4,30 @@ import (
 	"golang.org/x/net/webdav"
 	"net/http"
 	"log"
+	"time"
 )
+
+// todo: implement a custom webdav.FileSystem to store changes in git!
+// todo: implement a custom VERSIONS method (or extend PROPFIND) to list old versions
+// todo: maybe use a header on a GET to request old version?
 
 func RunWebDavServer(addr string, directory string) error {
 	webDavHandler := &webdav.Handler{
 		FileSystem: webdav.Dir(directory),
 		LockSystem: webdav.NewMemLS(),
 		Logger: func(r *http.Request, err error) {
-			log.Println("WD:", r.Method, r.RequestURI, "[" + r.RemoteAddr + "]", "|", err)
+			log.Println("WD", r.Method, r.RequestURI, "[" + r.RemoteAddr + "]", "|", err)
 		},
 	}
 	server := &http.Server{Addr: addr, Handler: corsWrapper(noCacheWrapper(webDavHandler))}
 	return server.ListenAndServe()
+}
+
+func slowWrapper(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "OPTIONS" { time.Sleep(4 * time.Second) }
+		h.ServeHTTP(w, r)
+	})
 }
 
 func corsWrapper(h http.Handler) http.Handler {
