9044b35c60b44e5811cae94a3b0b8f6d02b53e00
Brian Ewing <brianewing@gmail.com>
2018-01-17 03:04:42 +0000

    effects.LarsonEffect: add Speed parameter controlling oscillation frequency

--- effects/larson_effect.go
+++ effects/larson_effect.go
@@ -8,26 +8,17 @@ import (
 type LarsonEffect struct {
 	Color    []uint8
 	Position int
-	velocity int
+	Speed    float64
 }
 
 func (e *LarsonEffect) Render(s *strip.LEDStrip) {
-	if e.Position <= 1 || e.velocity == 0 {
-		e.velocity = 1
-	} else if e.Position >= s.Size-1 {
-		e.velocity = -1
+	if e.Speed != 0 {
+		e.Position = round(OscillateBetween(0, float64(s.Size-1), e.Speed))
 	}
 
 	color := e.getColor()
 	s.SetPixel(e.Position, color)
-
-	if e.Position != 0 {
-		s.SetPixel(e.Position-1, color)
-	} else {
-		s.SetPixel(e.Position+1, color)
-	}
-
-	e.Position += e.velocity
+	s.SetPixel(e.Position+1, color)
 }
 
 func (e *LarsonEffect) getColor() []uint8 {
