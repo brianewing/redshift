5f9b0284443027003c61e20d6a8083be69c9a770
Brian Ewing <brianewing@gmail.com>
2018-01-20 02:56:14 +0000

    Update built-in effect scripts

--- skel/scripts/rainbow.py
+++ skel/scripts/rainbow.py
@@ -1,47 +1,40 @@
 #!/usr/bin/python
 
 import colorsys, random, sys
-from redshift import run
+from redshift import run, log
 
-wheel = None
-size = 150
-speed = 1
-brightness = 1.0
-saturation = 1.0
+class Rainbow:
+    wheel = None
+    size = 150
+    speed = 1
+    brightness = 1.0
+    saturation = 1.0
+    
+    def __call__(self, frame):
+    	if self.wheel is None:
+    		self.wheel = self.generate_wheel(self.size or len(frame))
+    	else:
+    		self.wheel = self.wheel[-self.speed:] + self.wheel[:-self.speed]
+    		
+    	return self.wheel
+    
+    def generate_wheel(self, size):
+    	wheel = []
+    	
+    	for i in range(size):
+    		hue = float(i) / size
+    		rgb = colorsys.hsv_to_rgb(hue, self.saturation, self.brightness)
+    		wheel.append((int(255*rgb[0]), int(255*rgb[1]), int(255*rgb[2])))
+
+    	return wheel
+
+effect = Rainbow()
 
 try:
-	size = int(sys.argv[1])
+	effect.size = int(sys.argv[1])
+	effect.speed = int(sys.argv[2])
 except: pass
 
-def rainbow(frame):
-	global wheel, size, speed
-	if wheel is None:
-		wheel = generate_wheel(size or len(frame))
-	else:
-		wheel = wheel[-speed:] + wheel[:-speed]
-	adjust_speed()
-	if speed == 0:
-	    speed = 1
-	return wheel
+log("Starting rainbow effect, size=%r, speed=%r" % (effect.size, effect.speed))
 
-i = 0
-def adjust_speed():
-	global speed, i
-	if i % 150 == 0:
-		speed += random.randint(-1, 1)
-	elif i % 75 == 0 and random.randint(1, 2) == 1:
-		if speed > 3:
-			speed -= 1
-		elif speed < -3:
-			speed += 1
-	i += 1
-
-def generate_wheel(size):
-	wheel = []
-	for i in range(size):
-		hue = float(i) / size
-		rgb = colorsys.hsv_to_rgb(hue, saturation, brightness)
-		wheel.append((int(255*rgb[0]), int(255*rgb[1]), int(255*rgb[2])))
-	return wheel
-
-run(rainbow)
+run(effect)
--- skel/scripts/strip_redblue.js
+++ skel/scripts/strip_redblue.js
@@ -3,14 +3,14 @@ let redshift = require('./redshift')
 
 /*
  * Adapted from github.com/scanlime/fadecandy
- * (examples/javascript/strip_redblue.js)
+ * (examples/node/strip_redblue.js)
  */
 
 redshift((frame) => {
-	for (let i=0; i<frame.length; i++) {
-		let t = i * 0.2 + Date.now() * 0.002;
-		frame[i][0] = 128 + 96 * Math.sin(t);
-		frame[i][1] = 128 + 96 * Math.sin(t + 0.1);
-		frame[i][2] = 128 + 96 * Math.sin(t + 0.3);
-	}
+    for (let pixel=0; pixel<frame.length; pixel++) {
+        let t = pixel * 0.2 + Date.now() * 0.002;
+        frame[pixel][0] = 128 + 96 * Math.sin(t);
+        frame[pixel][1] = 128 + 96 * Math.sin(t + 0.1);
+        frame[pixel][2] = 128 + 96 * Math.sin(t + 0.3);
+    }
 });
