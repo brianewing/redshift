e8928a400d6bb4e67b622ca7f1de8c8d08416551
Brian Ewing <brianewing@gmail.com>
2018-03-27 02:06:00 +0100

    effects: fix json test

--- effects/json_test.go
+++ effects/json_test.go
@@ -7,7 +7,7 @@ import (
 func animationSet() []Effect {
 	return []Effect{
 		&Clear{},
-		&RainbowEffect{Size: 150, Speed: 1, Dynamic: true},
+		&RainbowEffect{Size: 150, Speed: 1},
 		&BlueEffect{},
 		&LarsonEffect{Color: []uint8{0,0,0}},
 	}
@@ -18,31 +18,31 @@ func BenchmarkMarshalJSON(b *testing.B) {
 		effects := animationSet()
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			MarshalJson(effects)
+			MarshalJSON(effects)
 		}
 	})
 	b.Run("Layer{Example}", func(b *testing.B) {
 		effects := []Effect{&Layer{Effects: animationSet()}}
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			MarshalJson(effects)
+			MarshalJSON(effects)
 		}
 	})
 }
 
 func BenchmarkUnmarshalJSON(b *testing.B) {
 	b.Run("Example", func(b *testing.B) {
-		effectsJson, _ := MarshalJson(animationSet())
+		effectsJson, _ := MarshalJSON(animationSet())
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			UnmarshalJson(effectsJson)
+			UnmarshalJSON(effectsJson)
 		}
 	})
 	b.Run("Layer{Example}", func(b *testing.B) {
-		effectsJson, _ := MarshalJson([]Effect{&Layer{Effects: animationSet()}})
+		effectsJson, _ := MarshalJSON([]Effect{&Layer{Effects: animationSet()}})
 		b.ResetTimer()
 		for i := 0; i < b.N; i++ {
-			UnmarshalJson(effectsJson)
+			UnmarshalJSON(effectsJson)
 		}
 	})
 }
