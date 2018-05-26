74da39b58933f033472ebbf0e533b20e07ebb62a
Brian Ewing <brianewing@gmail.com>
2018-05-26 13:41:51 +0100

    effects.Switch: call Init() on sub effects

--- effects/switch.go
+++ effects/switch.go
@@ -7,6 +7,10 @@ type Switch struct {
 	Selection int
 }
 
+func (e *Switch) Init() {
+	e.Effects.Init()
+}
+
 func (e *Switch) Render(s *strip.LEDStrip) {
 	if e.Selection < len(e.Effects) {
 		e.Effects[e.Selection].Render(s)
