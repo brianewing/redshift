fd6c7282ced431e625ed4cd6ceed3bc6c6a22fa4
Brian Ewing <me@brianewing.co.uk>
2019-08-02 16:33:37 +0100

    osc: Fix indentation in summary.go

--- osc/summary.go
+++ osc/summary.go
@@ -8,19 +8,19 @@ var summary = make(map[string]OscMessage) // osc address => last msg
 // returns a summary of the messages that have been received so far as a map,
 // where keys represent an osc address and values are the latest message received for that address
 func Summary() map[string]OscMessage {
-  summaryMutex.Lock()
-  _copy := make(map[string]OscMessage, len(summary))
-  for addr, msg := range summary {
-    _copy[addr] = msg
-  }
-  summaryMutex.Unlock()
+	summaryMutex.Lock()
+	_copy := make(map[string]OscMessage, len(summary))
+	for addr, msg := range summary {
+		_copy[addr] = msg
+	}
+	summaryMutex.Unlock()
 	return _copy
 }
 
 func ClearSummary() {
-  summaryMutex.Lock()
+	summaryMutex.Lock()
 	summary = make(map[string]OscMessage)
-  summaryMutex.Unlock()
+	summaryMutex.Unlock()
 }
 
 // streams messages the spool contained in this package
@@ -28,9 +28,9 @@ func ClearSummary() {
 func createSummary() {
 	msgs, _ := StreamMessages()
 	for msg := range msgs {
-    summaryMutex.Lock()
+		summaryMutex.Lock()
 		summary[msg.Address] = msg
-    summaryMutex.Unlock()
+		summaryMutex.Unlock()
 	}
 }
 
